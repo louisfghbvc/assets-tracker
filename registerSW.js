39a9f1cd42f801fe1c7b45c7fd1a66cd664db463
louisfghbvc <43661591+louisfghbvc@users.noreply.github.com>
2026-01-06 08:17:49 +0000

    Deploying to gh-pages from @ louisfghbvc/assets-tracker@459b754169cdc26b1f43d54f148292bec94eb71e 🚀

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/assets-tracker/sw.js', { scope: '/assets-tracker/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
