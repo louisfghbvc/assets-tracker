ca06fbfdf6aaef035124f21b6afb37df12ccf2ef
louisfghbvc <louisfghbvc@gmail.com>
2026-01-06 16:27:38 +0800

    feat: Configure Vite server and HMR host using TAURI_DEV_HOST environment variable.

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,9 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { VitePWA } from "vite-plugin-pwa";
 
+// @ts-expect-error process is a nodejs global
+const host = process.env.TAURI_DEV_HOST;
+
 // https://vite.dev/config/
 export default defineConfig(async () => ({
   plugins: [
@@ -40,12 +43,14 @@ export default defineConfig(async () => ({
   server: {
     port: 1420,
     strictPort: true,
-    host: "0.0.0.0",
-    hmr: {
-      protocol: "ws",
-      host: "0.0.0.0",
-      port: 1421,
-    },
+    host: host || false,
+    hmr: host
+      ? {
+        protocol: "ws",
+        host,
+        port: 1421,
+      }
+      : undefined,
     watch: {
       // 3. tell vite to ignore watching `src-tauri`
       ignored: ["**/src-tauri/**"],
